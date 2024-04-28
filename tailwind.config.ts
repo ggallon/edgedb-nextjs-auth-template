bac7808a20322dea556bd6a60084bf435184b92c
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-04-28 18:48:57 +0200

    Update configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./components/**/*.{ts,tsx,mdx}", "./app/**/*.{ts,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
@@ -24,4 +20,5 @@ const config: Config = {
   },
   plugins: [require("@tailwindcss/forms")],
 };
+
 export default config;
